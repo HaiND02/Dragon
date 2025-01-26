bbdb22f9e6d0481569f5cd88d31fe6e215b0c8b9
HaiND02 <haizaki505@gmail.com>
2025-01-26 14:02:05 +0700

    Initial Commit

---
+++ Dragon/DragonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonApp.swift
+//  Dragon
+//
+//  Created by Nguyễn Đình Hải on 26/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DragonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
